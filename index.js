96f64246192e5ff855206d812a613442a32baee7
Telvin <mark.telvin@student.moringaschool.com>
2024-03-27 10:36:36 +0300

    tr

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function() {
+    return 2000/100
+}
+const square = num => num * num;
+console.log(square(5));
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5
